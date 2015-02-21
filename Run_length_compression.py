81baa971655f88196e4253c66dd9aa6995304099
Vidit Jain <jain.vidit@outlook.com>
2015-02-21 07:38:20 +0530

    - UI for Run Length Compression

--- Run_length_compression.py
+++ Run_length_compression.py
@@ -1,38 +1,35 @@
 import Tkinter as Tk
+import Run_length_compression_algorithm as RLA
+import Run_Length as RL
 
-def RL_compress():
-    def prin():
-        print input_content.get()
+def run_length_compress():
+    def print_content():
+        rl_output.delete("1.0",Tk.END)
+        rl_output.insert("1.0",RLA.compress(rl_input.get("1.0",Tk.END)))
+
+    def Go_Back():
+        compression_window.destroy()
+        RL.run_length_menu()
+
     compression_window = Tk.Tk()
-    compression_window.geometry("400X400")
     compression_window.wm_title("Compression : ")
-    Tk.Label(compression_window,text="Enter input").grid(row = 0 )
+    Tk.Label(compression_window,text="Enter input").grid( row = 0 )
 
 
-    input_content = Tk.StringVar()
-    rl_input=Tk.Entry(compression_window,text="Enter aInput",textvariable = input_content,height=50,width= 50)
-    rl_input.grid(row=1,column=0)
-    rl_input.insert(0,"Enter text here")
+    rl_input=Tk.Text(compression_window,height=5,width=50)#,text="Enter Input")#,textvariable = input_content)
+    rl_input.grid(row=1,column=0,padx=10)
+    rl_input.insert("1.0","Enter text here")
 
-    input_button=Tk.Button(compression_window,text = "Click to decompress",command=prin).grid(row=2)
+    input_button=Tk.Button(compression_window,text = "Click to decompress",command=print_content).grid(row=2)
+
+    Tk.Label(compression_window,text="Compressed String is : ").grid( row = 4)
+    rl_output=Tk.Text(compression_window,height=5,width=50)
+    rl_output.grid(row=5)
+
+
+    exit_button=Tk.Button(compression_window,text = "Back",command=Go_Back).grid(row=6)
 
     compression_window.mainloop()
-RL_compress()
+    compression_window.lift()
+#run_length_compress()
 
-def analyze(event=None):
-    content = entry_contents.get()
-    if content == "":
-        entry_contents.set("default")
-
-lord = Tk.Tk()
-
-entry_contents = Tk.StringVar()
-aEntry = Tk.Entry(lord, textvariable=entry_contents)
-aEntry.grid()
-
-aText = Tk.Text(lord, font=("Georgia", "12"))
-aText.grid()
-
-aEntry.bind("<FocusOut>", analyze)
-
-lord.mainloop()
